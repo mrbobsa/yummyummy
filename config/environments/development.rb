8ceb0840a0800d0733421e920bcab94e8bde7563
binaryrain01 <binaryrain01@gmail.com>
2015-09-19 17:56:12 +0000

    precompile finish

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,4 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   
-  config.assets.precompile =  ['*.js', '*.css', '*.scss', '*.css.erb']
-  config.assets.compile = true
 end
